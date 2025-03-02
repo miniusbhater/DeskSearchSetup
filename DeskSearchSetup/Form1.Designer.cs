bd4e9f55c988e091a5876f1786ceecfd802eb2b3
evelyn <numpad0000@gmail.com>
2025-03-02 16:03:24 +0000

    Add files via upload

---
+++ DeskSearchSetup/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DeskSearchSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To DeskSearch Setup!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\Program Files (x86)\\DeskSearch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Install Location";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(369, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Install!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(15, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Run at startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(15, 132);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(99, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Run after install";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(120, 109);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Open README";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(120, 132);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(84, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Join Discord";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(384, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(328, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 9;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(227, 109);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(137, 17);
+            this.checkBox5.TabIndex = 10;
+            this.checkBox5.Text = "Install .net runtime (x64)";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(226, 132);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(137, 17);
+            this.checkBox6.TabIndex = 11;
+            this.checkBox6.Text = "Install .net runtime (x86)";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 158);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DeskSearch Setup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
---
+++ DeskSearchSetup/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using IWshRuntimeLibrary;
+
+namespace DeskSearchSetup
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string programfolder = textBox1.Text;
+            if (!Directory.Exists(programfolder))
+            {
+                Directory.CreateDirectory(programfolder);
+            }
+
+            // desksearch cool yaeh
+            string download = "https://github.com/miniusbhater/DeskSearch/raw/main/DeakSearch.exe";
+            string directory = textBox1.Text;
+            string file = "DeskSearch.exe";
+            string full = Path.Combine(directory, file);
+
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.DownloadProgressChanged += (s, ev) =>
+                {
+                    label1.Text = $"Please wait {ev.ProgressPercentage}%";
+                };
+                webclient.DownloadFileCompleted += (s, ev) =>
+                {
+                    label1.Text = "If you instaled .net wait";
+                    if (checkBox2.Checked)
+                    {
+                        Process.Start(full);
+                    }
+
+                    string shortcutName = "DeskSearch.lnk";
+                    string targetPath = @"C:\Program Files (x86)\DeskSearch\DeskSearch.exe";
+                    string startMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+                    string shortcutPath = Path.Combine(startMenuPath, "Programs", shortcutName);
+                    try
+                    {
+                        WshShell shell = new WshShell();
+                        IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+                        shortcut.TargetPath = targetPath;
+                        shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);
+                        shortcut.Save();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("something went beepy boopy wrong with making a start menu shortcut :(");
+                    }
+
+
+
+
+                }
+            ;
+                webclient.DownloadFileAsync(new Uri(download), full);
+            }
+
+            // readme download
+            string download2 = "https://raw.githubusercontent.com/miniusbhater/DeskSearch/main/README.txt";
+            string directory2 = textBox1.Text;
+            string file2 = "README.txt";
+            string full2 = Path.Combine(directory2, file2);
+
+            using (WebClient webclient2 = new WebClient())
+            {
+                webclient2.DownloadProgressChanged += (s, ev) =>
+                {
+                    label1.Text = $"Please wait {ev.ProgressPercentage}%";
+                };
+
+                webclient2.DownloadFileCompleted += (s, ev) =>
+                {
+                    label1.Text = "If you downloaded .net then wait";
+                    if (checkBox3.Checked)
+                    {
+                        Process.Start(full2);
+                    }
+                };
+                webclient2.DownloadFileAsync(new Uri(download2), full2);
+            }
+
+            // .net x64
+            if (checkBox5.Checked)
+            {
+                string download1 = "https://download.visualstudio.microsoft.com/download/pr/fc8c9dea-8180-4dad-bf1b-5f229cf47477/c3f0536639ab40f1470b6bad5e1b95b8/windowsdesktop-runtime-8.0.13-win-x64.exe";
+                string directory1 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string file1 = "windowsdesktop-runtime-8.0.13-win-x64.exe";
+                string full1 = Path.Combine(directory1, file1);
+
+                using (WebClient webclient1 = new WebClient())
+                {
+                    webclient1.DownloadFileCompleted += (s, ev) =>
+                    {
+                        if (ev.Error == null)
+                        {
+                            Process.Start(full1);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Download failed: " + ev.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    };
+                    webclient1.DownloadFileAsync(new Uri(download1), full1);
+                }
+            }
+
+            // .net x86
+            if (checkBox6.Checked)
+            {
+                string download11 = "https://download.visualstudio.microsoft.com/download/pr/b1827c52-ec83-4b3e-8d24-f321276bcdea/812e8d5871111cdc02cc82209c7d45fd/windowsdesktop-runtime-8.0.13-win-x86.exe";
+                string directory11 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string file11 = "windowsdesktop-runtime-8.0.13-win-x86.exe";
+                string full11 = Path.Combine(directory11, file11);
+
+                using (WebClient webclient11 = new WebClient())
+                {
+                    webclient11.DownloadFileCompleted += (s, ev) =>
+                    {
+                        if (ev.Error == null)
+                        {
+                            Process.Start(full11);
+                        }
+                    };
+                    webclient11.DownloadFileAsync(new Uri(download11), full11);
+                }
+            }
+
+            // Download again to startup because im lazy :P
+            if (checkBox1.Checked)
+            {
+                string download111 = "https://github.com/miniusbhater/DeskSearch/raw/main/DeakSearch.exe";
+                string directory111 = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+                string file111 = "DeskSearch.exe";
+                string full111 = Path.Combine(directory111, file111);
+
+                using (WebClient webclient111 = new WebClient())
+                {
+                    webclient111.DownloadProgressChanged += (s, ev) =>
+                    {
+                        label1.Text = $"Installing: {ev.ProgressPercentage}%";
+                    };
+
+                    webclient111.DownloadFileCompleted += (s, ev) =>
+                    {
+                        label1.Text = "Installed!";
+                    };
+                    webclient111.DownloadFileAsync(new Uri(download111), full111);
+                }
+            }
+
+            // Discord
+            if (checkBox4.Checked)
+            {
+                var open = new ProcessStartInfo
+                {
+                    FileName = "https://discord.gg/vgGVHhfBcf",
+                    UseShellExecute = true
+                };
+                Process.Start(open);
+
+
+
+
+            }
+        }
+        
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
